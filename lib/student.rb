f72bb99eece12bd9f322387dcec3551a27392e7d
Rafael Lopez <lpz.raf@gmail.com>
2018-11-25 22:48:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,10 @@ require_relative "../config/environment.rb"
 
 class Student
   
-  attr_accessor :name, :grade
-  attr_reader:id 
+  attr_accessor :id, :name, :grade
+
   def initialize(id = nil, name, grade) 
+    @id = id
     @name = name
     @grade = grade
   end
