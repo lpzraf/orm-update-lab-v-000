41ba98080c69340ea14f6ac161d7bf18234f734e
Rafael Lopez <lpz.raf@gmail.com>
2018-11-25 22:57:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,11 +61,12 @@ end
     DB[:conn].execute(sql, self.id, self.name, self.grade)
   end
   
-    def self.new_from_db(id, name, grade)
+    def self.new_from_db(row)
+      
       new_student = self.new  
-      new_student.id = id[0]
-      new_student.name =  name[1]
-      new_student.grade = grade[2]
+      new_student.id = row[0]
+      new_student.name =  row[1]
+      new_student.grade = row[2]
       new_student
   end
   
