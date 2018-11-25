eff4abef16f1ff0ce2ba01cc2c58352402ee94e2
Rafael Lopez <lpz.raf@gmail.com>
2018-11-25 23:04:19 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,10 @@ require_relative "../config/environment.rb"
 
 class Student
   
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :id, :name, :grade
+
   def initialize(id = nil, name, grade) 
+    @id = id
     @name = name
     @grade = grade
   end
@@ -29,5 +30,42 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) 
+        VALUES (?, ?)
+      SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+end
+  
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
+  
+   def update
+    sql = "UPDATE students SET id = ?, name = ? WHERE grade = ?"
+    DB[:conn].execute(sql, self.id, self.name, self.grade)
+  end
+  
+    def self.new_from_db(row)
+      id = row[0]
+      name =  row[1]
+      grade = row[2]
+      Student.new(id, name, grade)  
+  end
   
 end
